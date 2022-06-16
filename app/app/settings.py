f9b0e236a8a3e5347ab61ad20d03e0c42461b5a3
hspark <ssook7979@gmail.com>
2022-06-16 17:15:14 +0900

    fixed typo

--- app/app/settings.py
+++ app/app/settings.py
@@ -133,6 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
-REST_FRAMEWORD = {
+REST_FRAMEWORK = {
 	'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
